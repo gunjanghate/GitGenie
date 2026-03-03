3368fe5c3d1f4ab1b4086a8bf593cd4f4a9fc5ca
gunjanghate <ghategunjan@gmail.com>
2026-03-03 17:28:42 +0530

    Revert "feat: fixes in screen size"
    This reverts commit 552d54dc6e8ed939111a0cd8391bf34c7af94a90.

--- components/scroll-section.tsx
+++ components/scroll-section.tsx
@@ -13,7 +13,7 @@ export default function ScrollSection({
   // zIndex can be provided to ensure later sections stack on top
   return (
     <div className="scroll-stack-section" style={{ zIndex }}>
-      <div className="sticky-wrapper h-screen w-full">{children}</div>
+      <div className="sticky-wrapper min-h-screen w-full">{children}</div>
     </div>
   );
 }
