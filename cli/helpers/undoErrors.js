52594fcef70ff2ec6561533eacf101f8f622c009
Anurag Singh <singhanurag0317@gmail.com>
2026-06-05 14:13:50 +0530

    fix: handleUndoError classifies reflog/permission/lock errors and sanitizes home-dir paths (CWE-209, CWE-390)

--- cli/helpers/undoErrors.js
+++ cli/helpers/undoErrors.js
@@ -1,3 +1,5 @@
+// FIX Bug 4 (CWE-209/CWE-390): In handleUndoError generic fallback,
+// classify reflog/permission/lock errors and sanitize home-dir paths before printing.
 import chalk from 'chalk';
 
 /**
